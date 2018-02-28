22eef57eb2b0b5c160313f23e0f18242cdab5d43
fengmk2 <fengmk2@gmail.com>
2018-02-28 18:03:56 +0800

    backport: #2146 support Keep-Alive Header

--- app/middleware/meta.js
+++ app/middleware/meta.js
@@ -9,5 +9,12 @@ module.exports = () => {
     yield next;
     // total response time header
     this.set('x-readtime', Date.now() - this.starttime);
+
+    // try to support Keep-Alive Header
+    const server = this.app.server;
+    if (server && server.keepAliveTimeout && server.keepAliveTimeout >= 1000 && this.header.connection !== 'close') {
+      const timeout = parseInt(server.keepAliveTimeout / 1000);
+      this.set('keep-alive', `timeout=${timeout}`);
+    }
   };
 };
